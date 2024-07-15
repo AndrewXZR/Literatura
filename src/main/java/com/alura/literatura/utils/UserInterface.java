f634aad906a391748cc390dc39a31e51a952442e
Andres Ferra <andres.ferra@mi.unc.edu.ar>
2024-07-15 17:39:03 -0300

    clase UserInterface

---
+++ src/main/java/com/alura/literatura/utils/UserInterface.java
@@ -0,0 +1,21 @@
+package com.alura.literatura.utils;
+
+public class UserInterface {
+    public static void showWelcomeMessage() {
+        System.out.println("****************************************** Bienvenido a Literatura ***********" +
+                "******************************\n");
+    }
+
+    public static void messageLoop() {
+        System.out.println("*************************************************************************************" +
+                "**************************");
+        System.out.println("1 - Buscar libro por título");
+        System.out.println("2 - Listar libros registrados");
+        System.out.println("3 - Listar autores registrados");
+        System.out.println("4 - Listar autores vivos en un año determinado");
+        System.out.println("5 - Listar libros por idioma");
+        System.out.println("6 - Salir");
+        System.out.println("*************************************************************************************" +
+                "**************************");
+    }
+}
